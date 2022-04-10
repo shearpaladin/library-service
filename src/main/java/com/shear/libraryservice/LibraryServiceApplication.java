e22256a8f7e77b4137dc4566d215ddc032f435a1
shearpaladin <sandy6@ualberta.ca>
2022-04-10 17:22:59 -0600

    adding starter project items

---
+++ src/main/java/com/shear/libraryservice/LibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shear.libraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shear/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shear.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
